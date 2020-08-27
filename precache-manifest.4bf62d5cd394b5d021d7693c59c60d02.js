3041f9d13b940ed3c876bd75827ea71768d323b1
brunobog <bog906@gmail.com>
2020-08-27 15:50:24 +0000

    Updates

---
+++ precache-manifest.4bf62d5cd394b5d021d7693c59c60d02.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb631fbe9f92045195c1d2185b28791",
+    "url": "/Finance/index.html"
+  },
+  {
+    "revision": "5e3639e292fd2dbc5f40",
+    "url": "/Finance/static/css/2.35b1af77.chunk.css"
+  },
+  {
+    "revision": "4b8f3ee17f1d9d29e59c",
+    "url": "/Finance/static/css/main.20ca4495.chunk.css"
+  },
+  {
+    "revision": "5e3639e292fd2dbc5f40",
+    "url": "/Finance/static/js/2.6aeccb00.chunk.js"
+  },
+  {
+    "revision": "25f2e7d0aec957a4bcae49829afe2725",
+    "url": "/Finance/static/js/2.6aeccb00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b8f3ee17f1d9d29e59c",
+    "url": "/Finance/static/js/main.08dd1976.chunk.js"
+  },
+  {
+    "revision": "bc57943eade0922a8b49",
+    "url": "/Finance/static/js/runtime-main.5008a952.js"
+  },
+  {
+    "revision": "71e76e56c75e07bd1fa0f7d65dee69f6",
+    "url": "/Finance/static/media/cash-payment-bag-1.71e76e56.svg"
+  },
+  {
+    "revision": "1a271b19e3520a0661abc00c2b7b36c4",
+    "url": "/Finance/static/media/cifrao.1a271b19.svg"
+  }
+]);
